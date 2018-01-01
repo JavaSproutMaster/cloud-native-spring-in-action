9c824d0b31024dd4aba8ec9360776b0f8e247530
dancinguru <steadyblinder@gmail.com>
2018-01-01 00:57:25 -0500

    add config...

---
+++ src/main/java/com/sunshineoxygen/inhome/config/FlywayMigrationStrategyImpl.java
@@ -0,0 +1,12 @@
+package com.sunshineoxygen.inhome.config;
+import org.flywaydb.core.Flyway;
+import org.springframework.boot.autoconfigure.flyway.FlywayMigrationStrategy;
+import org.springframework.stereotype.Component;
+
+@Component
+public class FlywayMigrationStrategyImpl implements FlywayMigrationStrategy {
+    @Override
+    public void migrate(Flyway flyway) {
+        flyway.migrate();
+    }
+}
